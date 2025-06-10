5e276e37fd0a78e6a3396ec2c372b3fda45cdd97
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-10 11:56:38 +0500

    add: close module method.

---
+++ close.go
@@ -0,0 +1,18 @@
+package kalkan
+
+/*
+#cgo LDFLAGS: -ldl
+#include <stdlib.h>
+#include <dlfcn.h>
+*/
+import "C"
+import "fmt"
+
+// Closes a Module.
+func (m *Module) Close() error {
+	C.dlclose(m.h)
+	if err := C.dlerror(); err != nil {
+		return fmt.Errorf("%s", C.GoString(err))
+	}
+	return nil
+}
