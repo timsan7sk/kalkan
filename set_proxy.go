ed8ef027b293f0312c6db1587187b45f2eb17fb3
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-10 17:07:39 +0500

    add: SetProxy().

---
+++ set_proxy.go
@@ -0,0 +1,44 @@
+package kalkan
+
+/*
+#cgo LDFLAGS: -ldl
+#include <dlfcn.h>
+#include "KalkanCrypt.h"
+
+unsigned long set_proxy(int flags, char *inProxyAddr, char *inProxyPort, char *inUser, char *inPass) {
+	return kc_funcs->KC_SetProxy(flags, inProxyAddr, inProxyPort, inUser, inPass);
+}
+*/
+
+import "C"
+import (
+	"fmt"
+	"net/url"
+	"unsafe"
+)
+
+func (m *Module) SetProxy(flag Flag, url *url.URL) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if url == nil {
+		return fmt.Errorf("%s", "URL must not be nil.")
+	}
+	cAddr := C.CString(url.Hostname())
+	defer C.free(unsafe.Pointer(cAddr))
+
+	cPort := C.CString(url.Port())
+	defer C.free(unsafe.Pointer(cPort))
+
+	cUser := C.CString(url.User.Username())
+	defer C.free(unsafe.Pointer(cProxyUser))
+
+	pass, _ := url.User.Password()
+	cPass := C.CString(pass)
+	defer C.free(unsafe.Pointer(cProxyPass))
+
+	rc := int(C.set_proxy(C.int(int(flag)), cAddr, cPort, cUser, cPass))
+
+	return m.wrapError(rc)
+
+}
