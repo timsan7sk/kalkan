a9aaed40e0fe039db180f5cdd02efccd1647568d
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-11 14:42:21 +0500

    add: VerifyXML() verifies the signature of XML data.

---
+++ verify_xml.go
@@ -0,0 +1,40 @@
+package kalkan
+
+/*
+#cgo LDFLAGS: -ldl
+#include <dlfcn.h>
+#include "KalkanCrypt.h"
+
+unsigned long verify_xml(char *alias, int flags, char *inData, int inDataLength, char *outVerifyInfo, int *outVerifyInfoLen) {
+	return kc_funcs->VerifyXML(alias, flags, inData, inDataLength, outVerifyInfo, outVerifyInfoLen);
+}
+*/
+import "C"
+import "unsafe"
+
+// Verifies the signature of XML data.
+func (m *Module) VerifyXML(alias, inData string, flags Flag) (string, error) {
+	// locking the module and unlocking it after completion.
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	// preparing variables and freeing memory when finished.
+	cAlias := C.CString(alias)
+	defer C.free(unsafe.Pointer(cAlias))
+
+	cInData := C.CString(inData)
+	defer C.free(unsafe.Pointer(cInData))
+
+	outVerifyInfoLen := 64768
+	outVerifyInfo := C.malloc(C.ulong(C.sizeof_char * outVerifyInfoLen))
+	defer C.free(outVerifyInfo)
+
+	// verifing XML data.
+	rc := int(C.verify_xml(cAlias, C.int(flags), cInData, C.int(len(inData)), (*C.char)(outVerifyInfo), (*C.int)(unsafe.Pointer(&outVerifyInfoLen))))
+
+	err := m.wrapError(rc)
+	if err != nil {
+		return "", err
+	}
+	return C.GoString((*C.char)(outVerifyInfo)), nil
+}
