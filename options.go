2b43a03e8f533a496130ba073139c92275a08186
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-11 15:55:19 +0500

    add: setDelaults() to options.go

--- options.go
+++ options.go
@@ -1,12 +1,37 @@
 package kalkan
 
+const (
+	prodOCSP = "http://ocsp.pki.gov.kz" // Production OCSP Service.
+	prodTSP  = "http://tsp.pki.gov.kz"  // Production TSP Service.
+
+	testOCSP = "http://test.pki.gov.kz/ocsp/" // Test OCSP Service.
+	testTSP  = "http://test.pki.gov.kz/tsp/"  // Test TSP Service.
+)
+
 type Options struct {
-	OCSP  string  `json:"ocsp"`  // URL of the online service for checking the certificate status.
-	TSP   string  `json:"tsp"`   // URL of the timestamp service.
-	Certs []Certs `json:"certs"` // Root CA certificates and intermediate CA certificates.
+	OCSP      string  `json:"ocsp"`  // URL of the online service for checking the certificate status.
+	TSP       string  `json:"tsp"`   // URL of the timestamp service.
+	Certs     []Certs `json:"certs"` // Root CA certificates and intermediate CA certificates.
+	LoadCerts bool    // Do need to load root CA certificates and intermediate CA certificates.
 }
 
 // Gets module options.
 func (m *Module) Options() Options {
 	return m.o
+}
+
+func (o *Options) setDefaults() {
+	if o == nil {
+		return
+	}
+
+	if o.TSP == "" {
+		o.TSP = prodTSP
+	}
+
+	if o.OCSP == "" {
+		o.OCSP = prodOCSP
+	}
+
+	o.LoadCerts = true
 }
