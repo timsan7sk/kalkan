23e72ad9fafd1a52ef58709917a65cc8677b63f5
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-11 14:59:37 +0500

    edit: embed test certs in test_main.go

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,12 +1,25 @@
 package tests
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"testing"
 
 	"pki.gov.kz/go/kalkan"
 )
+
+//go:embed gost1.p12
+var testKeyGOST1 []byte
+
+//go:embed gost1.cer
+var testCertGOST1 string
+
+//go:embed gost2.p12
+var testKeyGOST2 []byte
+
+//go:embed gost2.cer
+var testCertGOST2 string
 
 func TestMain(m *testing.M) {
 	mod, err := kalkan.Open("libkalkancryptwr-64.so.2")
