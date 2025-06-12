e19ab047e60a0af72d810f2e240b8f3f687637ed
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-12 17:10:45 +0500

    add: GetTimeFromSign() obtains the signature time.

---
+++ get_time_from_sign.go
@@ -0,0 +1,37 @@
+package kalkan
+
+/*
+#cgo LDFLAGS: -ldl
+#include <dlfcn.h>
+#include "KalkanCrypt.h"
+
+unsigned long get_time_from_sign(char *inData, int inDataLength, int flags, int inSigId, time_t *outDateTime) {
+    return kc_funcs->KC_GetTimeFromSig(inData, inDataLength, flags, inSigId, outDateTime);
+}
+*/
+import "C"
+import (
+	"time"
+	"unsafe"
+)
+
+// Obtains the signature time.
+func (m *Module) GetTimeFromSign(cms string, sigID int, flag Flag) (time.Time, error) {
+	// locking the module and unlocking it after completion.
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	// preparing variable and freeing memory when finished.
+	cCMS := C.CString(cms)
+	defer C.free(unsafe.Pointer(cCMS))
+
+	outDateTime := C.time_t(0)
+	rc := int(C.get_time_from_sign(cCMS, C.int(len(cms)), C.int(int(flag)), C.int(sigID), &outDateTime))
+
+	err := m.wrapError(rc)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Unix(int64(outDateTime), 0).UTC(), nil
+
+}
--- options.go
+++ options.go
@@ -1,18 +1,18 @@
 package kalkan
 
 const (
-	prodOCSP = "http://ocsp.pki.gov.kz" // Production OCSP Service.
-	prodTSP  = "http://tsp.pki.gov.kz"  // Production TSP Service.
+	ProdOCSP = "http://ocsp.pki.gov.kz" // Production OCSP Service.
+	ProdTSP  = "http://tsp.pki.gov.kz"  // Production TSP Service.
 
-	testOCSP = "http://test.pki.gov.kz/ocsp/" // Test OCSP Service.
-	testTSP  = "http://test.pki.gov.kz/tsp/"  // Test TSP Service.
+	TestOCSP = "http://test.pki.gov.kz/ocsp/" // Test OCSP Service.
+	TestTSP  = "http://test.pki.gov.kz/tsp/"  // Test TSP Service.
 )
 
 type Options struct {
-	OCSP      string  `json:"ocsp"`  // URL of the online service for checking the certificate status.
-	TSP       string  `json:"tsp"`   // URL of the timestamp service.
-	Certs     []Certs `json:"certs"` // Root CA certificates and intermediate CA certificates.
-	LoadCerts bool    // Do need to load root CA certificates and intermediate CA certificates.
+	OCSP      string  `json:"ocsp"`       // URL of the online service for checking the certificate status.
+	TSP       string  `json:"tsp"`        // URL of the timestamp service.
+	Certs     []Certs `json:"certs"`      // Root CA certificates and intermediate CA certificates.
+	LoadCerts bool    `json:"load_certs"` // Do need to load root CA certificates and intermediate CA certificates.
 }
 
 // Gets module options.
@@ -26,11 +26,11 @@ func (o *Options) setDefaults() {
 	}
 
 	if o.TSP == "" {
-		o.TSP = prodTSP
+		o.TSP = ProdTSP
 	}
 
 	if o.OCSP == "" {
-		o.OCSP = prodOCSP
+		o.OCSP = ProdOCSP
 	}
 
 	o.LoadCerts = true
