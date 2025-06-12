a40021ed5a4fbbaba8b474e016788c22bf2f43c2
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-12 17:40:12 +0500

    upd: TSASetURL() checking URL is valid.

--- set_tsa_url.go
+++ set_tsa_url.go
@@ -11,18 +11,24 @@ void set_tsa_url(char *tsaurl) {
 */
 import "C"
 import (
+	"net/url"
 	"unsafe"
 )
 
 // Sets the TSA service address.
-func (m *Module) TSASetURL(url string) error {
+func (m *Module) TSASetURL(tsaURL string) error {
+	// checking URL is valid.
+	_, err := url.ParseRequestURI(tsaURL)
+	if err != nil {
+		return err
+	}
 	// locking the module and unlocking it after completion.
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	// preparing variable and freeing memory when finished.
-	cTSA := C.CString(url)
-	defer C.free(unsafe.Pointer(cTSA))
+	cURL := C.CString(tsaURL)
+	defer C.free(unsafe.Pointer(cURL))
 	// set up a TSA service address.
-	C.set_tsa_url(cTSA)
+	C.set_tsa_url(cURL)
 	return nil
 }
