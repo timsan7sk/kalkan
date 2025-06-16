43da796ee216d2229c861cf3c3f0f84d7345f177
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-16 17:54:01 +0500

    add: X509LoadCertificateFromBuffer() Loads a certificate from a bite slice.

---
+++ x509_load_cert_from_buffer.go
@@ -0,0 +1,25 @@
+package kalkan
+
+/*
+#cgo LDFLAGS: -ldl
+#include <dlfcn.h>
+#include "KalkanCrypt.h"
+
+int x509_load_certificate_from_buffer(char *inCert, int certLength, int flag) {
+    return kc_funcs->X509LoadCertificateFromBuffer((unsigned char*)inCert, certLength, flag);
+}
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+// Loads a certificate from a bite slice.
+func (m *Module) X509LoadCertificateFromBuffer(inCert []byte, flags CertEncodingType) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	rc := int(C.x509_load_certificate_from_buffer((*C.char)(unsafe.Pointer(&inCert)), C.int(len(inCert)), C.int(int(flags))))
+
+	return m.wrapError(rc)
+}
