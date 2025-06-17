856be68ab5ae8f595826bb032d5a1fc04bc334da
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-17 15:54:39 +0500

    add: structs soapEnvelope, soapBody and neccesary consts, WrapSOAP() Wraps the XML document in SOAP format, or more precisely writes the contents under the soap:Body tag.

--- sign_wsse.go
+++ sign_wsse.go
@@ -10,7 +10,30 @@ package kalkan
 	}
 */
 import "C"
-import "unsafe"
+import (
+	"encoding/xml"
+	"unsafe"
+)
+
+const (
+	xmlnsSOAP = "http://schemas.xmlsoap.org/soap/envelope/"
+	xmlnsWSU  = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+	replace   = "replace"
+)
+
+// Represents soap:Envelope
+type soapEnvelope struct {
+	XMLName xml.Name `xml:"soap:Envelope"`
+	SOAP    string   `xml:"xmlns:soap,attr"`
+	WSU     string   `xml:"xmlns:wsu,attr"`
+	Body    soapBody `xml:"soap:Body"`
+}
+
+// Represents soap:Body
+type soapBody struct {
+	ID      string `xml:"wsu:Id,attr"`
+	Content string `xml:",chardata"`
+}
 
 // Signs SOAP messages according to the WS-Security specification. Required for [SmartBridge]:https://sb.egov.kz/.
 func (m *Module) SignWSSE(alias, inData, signNodeID string, flags Flag) (string, error) {
--- tools.go
+++ tools.go
@@ -1,5 +1,10 @@
 package kalkan
 
+import (
+	"encoding/xml"
+	"strings"
+)
+
 // Trims the slice.
 func trimSlice(data []byte) []byte {
 	for i, v := range data {
@@ -8,4 +13,24 @@ func trimSlice(data []byte) []byte {
 		}
 	}
 	return data
+}
+
+// Wraps the XML document in SOAP format, or more precisely writes the contents under the soap:Body tag.
+func WrapSOAP(inData, id string) (string, error) {
+	// Creating SOAP wrapper.
+	e := soapEnvelope{
+		SOAP: xmlnsSOAP,
+		WSU:  xmlnsWSU,
+		Body: soapBody{
+			ID:      id,
+			Content: replace,
+		},
+	}
+	// Marhaling SOAP wrapper.
+	b, err := xml.Marshal(e)
+	if err != nil {
+		return "", err
+	}
+	// Adding data to the soap:Body tag and return result.
+	return strings.Replace(string(b), replace, inData, 1), nil
 }
