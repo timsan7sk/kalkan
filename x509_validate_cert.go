c1630731556368a3450779c9dbfe5c13f0f8610f
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-17 09:26:10 +0500

    add: X509ValidateCertificate()  Validates the certificate.

---
+++ x509_validate_cert.go
@@ -0,0 +1,43 @@
+package kalkan
+
+// #cgo LDFLAGS: -ldl
+// #include <dlfcn.h>
+// #include <strings.h>
+// #include "KalkanCrypt.h"
+//
+// int x509_validate_certificate(char *inCert, int inCertLength, int validType, char *validPath, long long checkTime, char *outInfo, int *outInfoLength, int flags, char* getResp, int *getRespLength) {
+//     int rc = kc_funcs->X509ValidateCertificate(inCert, inCertLength, validType, validPath, checkTime, outInfo, outInfoLength, flags, getResp, getRespLength);
+//     for(size_t i = 0; i < *outInfoLength-1; i++)
+//         if(outInfo[i] == '\r' && outInfo[i+1] != '\n') outInfo[i] = '\n';
+//     return rc;
+// }
+import (
+	"unsafe"
+)
+
+const (
+	length = 8192
+)
+
+// Validates the certificate.
+func (m *Module) X509ValidateCertificate(inCert string, validateType ValidateType, validatePath string) (string, error) {
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	cInCert := C.CString(inCert)
+	defer C.free(unsafe.Pointer(cInCert))
+
+	cValidPath := C.CString(validatePath)
+	defer C.free(unsafe.Pointer(cValidPath))
+
+	var kcOutInfo [length]byte
+	kcOutInfoLen := length
+
+	var kcGetResp [length]byte
+	kcGetRespLen := length
+
+	rc := int(C.x509_validate_certificate(cInCert, C.int(len(inCert)), C.int(int(validateType)), cValidPath, 0, (*C.char)(unsafe.Pointer(&kcOutInfo)), (*C.int)(unsafe.Pointer(&kcOutInfoLen)), 0, (*C.char)(unsafe.Pointer(&kcGetResp)), (*C.int)(unsafe.Pointer(&kcGetRespLen))))
+
+	return string(byteSlice(kcOutInfo[:])), m.wrapError(rc)
+}
