a05c6fcdef5dd9eaa531740fb4acd90c79cbbcf1
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-19 11:03:13 +0500

    add: TestSignWSSE().

--- tests/sign_test.go
+++ tests/sign_test.go
@@ -15,6 +15,17 @@ func TestSignXML(t *testing.T) {
 	_ = testSignXML(t)
 }
 
+func TestSignWSSE(t *testing.T) {
+	data, err := kalkan.WrapSOAP("<test>data</test>", "id-ASDASDAS7786SA9S6D78A6SD9876ASD6")
+	if err != nil {
+		t.Fatal(err)
+	}
+	sData, err := mod.SignWSSE("", data, "", 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("WSSE signed data: ", sData)
+}
 func testSignData(t *testing.T) string {
 	data := "QOlCN2P7QVHU5mqcH"
 	sData, err := mod.SignData("", data, "", flags)
