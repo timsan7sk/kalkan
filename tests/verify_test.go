b4e970910076bb2aab45bc706e600f964ca3cb68
Timur A. Issanov <timur.issanov@nitec.kz>
2025-06-19 11:33:42 +0500

    add: TestVerifyXML().

--- tests/verify_test.go
+++ tests/verify_test.go
@@ -4,10 +4,19 @@ import "testing"
 
 func TestVerifyData(t *testing.T) {
 	sData := testSignData(t)
-	t.Log(sData)
 	vData, err := mod.VerifyData("", sData, "", flags)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log("Verified data: ", vData)
+}
+
+func TestVerifyXML(t *testing.T) {
+	sData := testSignXML(t)
+	vData, err := mod.VerifyXML("", sData, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Verified XML: ", vData)
+
 }
